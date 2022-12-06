83511b9efdcb667c10826cc63526508a3ee6bb8b
efimov-top-academy <efimov_m@t.top-academy.ru>
2022-12-06 15:39:47 +0300

    Добавьте файлы проекта.

---
+++ DPFlyweightProject/House.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPFlyweightProject
+{
+    abstract class House
+    {
+        protected int stages;
+        public abstract void Build(double x, double y);
+    }
+
+    class PanelHouse : House
+    {
+        public PanelHouse()
+        {
+            stages = 9;
+        }
+
+        public override void Build(double x, double y)
+        {
+            Console.WriteLine($"Panel House building on x:{x}, y:{y}");
+        }
+    }
+
+    class BrickHouse : House
+    {
+        public BrickHouse()
+        {
+            stages = 5;
+        }
+
+        public override void Build(double x, double y)
+        {
+            Console.WriteLine($"Brick House building on x:{x}, y:{y}");
+        }
+    }
+
+    class HouseFactory
+    {
+        Dictionary<string, House> housesPool = new();
+        public HouseFactory()
+        {
+
+        }
+
+        public House GetHouse(string key)
+        {
+            House house = null;
+            if (!housesPool.ContainsKey(key))
+            {  
+                switch (key)
+                {
+                    case "panel": 
+                        house = new PanelHouse();
+                        Console.WriteLine("Panel House Construct");
+                        break;
+                    case "brick": 
+                        house = new BrickHouse();
+                        Console.WriteLine("Brick House Construct");
+                        break;
+                    default:
+                        break;
+                } 
+                housesPool.Add(key, house);
+            }
+
+            return housesPool[key];
+        }
+    }
+}
---
+++ DPFlyweightProject/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace DPFlyweightProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 20;
+            double y = 30;
+
+            HouseFactory factory = new();
+            House house = null;
+            for(int i = 0; i < 10; i++)
+            {
+                Random rand = new();
+                switch(rand.Next(2))
+                {
+                    case 0: house = factory.GetHouse("panel"); break;
+                    case 1: house = factory.GetHouse("brick"); break;
+                    default:
+                        break;
+                }
+                if(house is not null)
+                    house.Build(x, y);
+                x += rand.Next(-10, 10);
+                y += rand.Next(-10, 10);
+            }
+        }
+    }
+}
